e605f3e104065b7c55049ff682fa5dbabf9fdf42
Jack Gage <jgage930@gmail.com>
2024-06-20 22:11:31 -0500

    prompt for password

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 pub mod args;
+pub mod models;
 
 use args::Args;
 use clap::Parser;
+use models::prompt_for_password;
 
 fn main() {
     let args = Args::parse();
 
     match args {
         Args::Add => {
-            println!("Got add")
+            let password = prompt_for_password().expect("Could not read input from command line.");
+            dbg!(password);
         }
     }
 }
