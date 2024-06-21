58497296644e0f0cb667c8d1ff17c5ea32c1312b
Jack Gage <jgage930@gmail.com>
2024-06-20 22:26:14 -0500

    connect to db

--- src/database.rs
+++ src/database.rs
@@ -4,10 +4,14 @@ use rusqlite::Connection;
 /// A trait to allow a struct to interact with a database.
 pub trait DbBase {
     // Create table in db.
-    fn create_table() -> Result<()>;
+    fn create_table(conn: &Connection) -> Result<()>;
 
     // Insert a new row into db.
     fn insert(&self, conn: &Connection) -> Result<Self>
     where
         Self: Sized;
+}
+
+pub fn connect() -> Connection {
+    Connection::open("data.db").expect("Could not connect to the database.")
 }
