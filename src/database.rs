0238475e3cf0c1ed56d642c9fbb23e7042b52754
Jack Gage <jgage930@gmail.com>
2024-06-20 22:22:03 -0500

    trait for db

--- src/database.rs
+++ src/database.rs
@@ -1,2 +1,13 @@
 use anyhow::Result;
 use rusqlite::Connection;
+
+/// A trait to allow a struct to interact with a database.
+pub trait DbBase {
+    // Create table in db.
+    fn create_table() -> Result<()>;
+
+    // Insert a new row into db.
+    fn insert(&self, conn: &Connection) -> Result<Self>
+    where
+        Self: Sized;
+}
