75a7244882a243d1d2f68e14563a6c46ac61e11e
Jack Gage <jgage930@gmail.com>
2024-09-05 09:39:17 -0500

    move args into functions

--- src/rups/run.rs
+++ src/rups/run.rs
@@ -14,10 +14,44 @@ fn add_password(db: &Connection) -> Result<()> {
     let password = prompt_for_password().expect("Could not read input from command line.");
 
     let id = password
-        .insert(&db)
+        .insert(db)
         .context("Failed to insert new password entry")?;
 
     println!("Inserted password {id}");
+
+    Ok(())
+}
+
+fn list_passwords(db: &Connection) -> Result<()> {
+    let passwords = Password::list(&db)?;
+
+    let table = Table::new(passwords).to_string();
+    println!("{table}");
+
+    Ok(())
+}
+
+fn get_password(id: i64, db: &Connection) -> Result<()> {
+    let password = Password::get_by_id(id, db)?;
+
+    let table = Table::new(password).to_string();
+    println!("{table}");
+
+    Ok(())
+}
+
+fn search_passwords(db: &Connection) -> Result<()> {
+    let completer = PasswordCompleter::new(db.into());
+
+    let search_val = Text::new("Enter Name: ")
+        .with_autocomplete(completer)
+        .prompt()?;
+
+    let new_db = connect();
+    let passwords = Password::get_by_col("name", &search_val, &new_db)?;
+
+    let table = Table::new(passwords).to_string();
+    println!("{table}");
 
     Ok(())
 }
@@ -27,41 +61,11 @@ pub fn run() -> Result<()> {
     let args = Args::parse();
 
     match args {
-        Args::Add => {
-            let password = prompt_for_password().expect("Could not read input from command line.");
-
-            let id = password
-                .insert(&db)
-                .context("Failed to insert new password entry")?;
-
-            println!("Inserted password {id}");
-        }
-        Args::Get { id } => {
-            let password = Password::get_by_id(id, &db)?;
-
-            let table = Table::new(password).to_string();
-            println!("{table}")
-        }
-        Args::List => {
-            let passwords = Password::list(&db)?;
-
-            let table = Table::new(passwords).to_string();
-            println!("{table}")
-        }
-        Args::Search => {
-            let completer = PasswordCompleter::new(db.into());
-
-            let search_val = Text::new("Enter Name: ")
-                .with_autocomplete(completer)
-                .prompt()?;
-
-            let new_db = connect();
-            let passwords = Password::get_by_col("name", &search_val, &new_db)?;
-
-            let table = Table::new(passwords).to_string();
-            println!("{table}")
-        }
-    }
+        Args::Add => add_password(&db),
+        Args::Get { id } => get_password(id, &db),
+        Args::List => list_passwords(&db),
+        Args::Search => todo!(),
+    };
 
     Ok(())
 }
